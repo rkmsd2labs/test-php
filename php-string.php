5d97f2761a5fc38f87e28a42e74e18de3cf4ea8a
rajumishra <rajukumar.mishra@sd2labs.com>
2017-01-19 14:52:41 +0530

    updated by master

--- php-string.php
+++ php-string.php
@@ -1,15 +1,8 @@
 <?php
  echo "Hi there!";
-<<<<<<< HEAD
- echo "Hi Git";
- echo "Hi Git2";
- echo "Hi git3";
-
- echo "added by test-u2";
-=======
  //echo "Hi Git";
  //echo "Hi Git2";
  //echo "Hi git3";
  echo 'updated by rkm';
->>>>>>> master
+ echo "added by test-u2";
 ?>
